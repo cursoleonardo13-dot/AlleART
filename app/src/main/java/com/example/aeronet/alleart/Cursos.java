1c3be36a558eb2e07ea8a03c821b41d31b2db959
cursoleonardo13-dot <cursoleonardo13@gmail.com>
2026-06-13 14:53:35 -0300

    first commit

--- app/src/main/java/com/example/aeronet/alleart/Cursos.java
+++ app/src/main/java/com/example/aeronet/alleart/Cursos.java
@@ -1,16 +1,49 @@
 package com.example.aeronet.alleart;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class Cursos extends AppCompatActivity {
 
+    Intent nav;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cursos);
     }
-    public void ClickVoltar(View view){
+
+    public void ClickVoltar(View view) {
         finish();
-}
+    }
+
+    public void clickCurPortugol(View view) {
+        nav = new Intent(Intent.ACTION_VIEW, Uri.parse("https://alleartcursos.com.br/"));
+        startActivity(nav);
+    }
+    public void clickCurAndroid(View view) {
+        nav = new Intent(Intent.ACTION_VIEW, Uri.parse("https://alleartcursos.com.br/"));
+        startActivity(nav);
+    }
+    public void clickCurPHP(View view) {
+        nav = new Intent(Intent.ACTION_VIEW, Uri.parse("https://alleartcursos.com.br/"));
+        startActivity(nav);
+    }
+    public void clickCurCSharp(View view) {
+        nav = new Intent(Intent.ACTION_VIEW, Uri.parse("https://alleartcursos.com.br/"));
+        startActivity(nav);
+    }
+    public void clickCurKotlin(View view) {
+        nav = new Intent(Intent.ACTION_VIEW, Uri.parse("https://alleartcursos.com.br/"));
+        startActivity(nav);
+    }
+    public void clickCurVideo(View view) {
+        nav = new Intent(Intent.ACTION_VIEW, Uri.parse("https://alleartcursos.com.br/"));
+        startActivity(nav);
+    }
+
+    }
--- app/src/main/java/com/example/aeronet/alleart/Midia.java
+++ app/src/main/java/com/example/aeronet/alleart/Midia.java
@@ -1,15 +1,27 @@
 package com.example.aeronet.alleart;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.VideoView;
 
 public class Midia extends AppCompatActivity {
+
+    VideoView video;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_midia);
+
+        video = (VideoView) findViewById(R.id.videoView);
+
+        String caminho = "android.resouce://br.com.alleartcursos.alleart/" + R.raw.videocursos;
+        Uri u = Uri.parse(caminho);
+        video.setVideoURI(u);
+
+        video.start();
     }
 
     public void clickVoltar(View view) {
